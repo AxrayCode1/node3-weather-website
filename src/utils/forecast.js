be8a2d55079fac91ed675f5aba68151f7f283166
SHIHCHANG <axray@hotmail.com>
2020-03-18 23:39:07 +0800

    Add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,8 @@ const forecast = (latitude,longtitude,callback) => {
             callback('Unable to connect to weather sevice!',undefined);
         } else if (body.error){
             callback('Unable to find location',undefined);
-        } else {
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. Theres is a ' + body.currently.precipProbability + '% chance of rain')
+        } else {            
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '.  Theres is a ' + body.currently.precipProbability + '% chance of rain')
         }
     })
 }
